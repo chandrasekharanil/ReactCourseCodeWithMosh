056f13866681f79c7ed2546c25a23335996165b9
chandrasekharanil <chandrasekharanil@gmail.com>
2020-09-24 15:34:07 -0500

    Mac Laptop Code
    Latest code from Mac

--- counter-app/src/components/counter.jsx
+++ counter-app/src/components/counter.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state={
+    count = 0,
+  };
   render() {
     return (
       <div>
-        <h1>Learning React!</h1>
+        <h1>{this.state.count}</h1>
         <button>Increment!</button>
       </div>
     );
